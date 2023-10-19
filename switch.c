b40525af1750731fc091470be341c351152e90c5
hmasood21955 <hmasood878@gmail.com>
2023-10-19 10:56:29 +0500

    copy by backgroud and program will run

--- switch.c
+++ switch.c
@@ -4,4 +4,22 @@ int main()
     int classLevel;
     printf("eneter the num");
     scanf("%d",& classLevel);
+    switch(classLevel)
+  {
+  case 1:
+    printf("Freshman");
+    break;
+  case 2:
+    printf("Sophomore");
+    break;
+  case 3:
+    printf("Junior");
+    break;
+  case 4:
+    printf("Senior");
+    break;
+  default:
+    printf("ERROR: Unknown value!");
+    break;
+   }
 }
